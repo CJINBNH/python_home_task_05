345f89365c5a4c70a340c31cec3fefb90beca394
CJINBNH <alex_egorov_89@mail.ru>
2022-10-14 17:52:59 +0400

    Добавил решение задачи №1

--- 001.py
+++ 001.py
@@ -4,3 +4,80 @@
 # Все конфеты оппонента достаются сделавшему последний ход. Сколько конфет нужно взять первому игроку,
 # чтобы забрать все конфеты у своего конкурента?
 # a) Добавьте игру против бота
+
+import random
+print('Не останься последним!')
+sweets = 117
+corch = random.randint(0, 1)
+while sweets > 0:
+    if corch == 1:
+        yourMove = int(input('Сколько конфет возьмёте? - '))
+        if 1 <= yourMove < 29:
+            sweets -= yourMove
+            print("осталось: ", sweets)
+            corch = 0
+        else:
+            print('Взять можно не более 28 конфет')
+            corch = 1
+    else:
+        if 114 < sweets <= 142:
+            comp = 2
+            sweets -= comp
+            print('Компьютер берёт', comp)
+            print('осталось', sweets)
+            corch = 1
+        elif 86 < sweets <= 114:
+            comp = sweets - 87
+            if comp >= 1:
+                sweets -= comp
+                print('Компьютер берёт', comp)
+                print('осталось', sweets)
+                corch = 1
+            else:
+                comp += 1
+                sweets -= comp
+                print('Компьютер берёт', comp)
+                print('осталось', sweets)
+                corch = 1
+        elif 58 < sweets <= 86:
+            comp = sweets - 59
+            if comp >= 1:
+                sweets -= comp
+                print('Компьютер берёт', comp)
+                print('осталось', sweets)
+                corch = 1
+            else:
+                comp += 1
+                sweets -= comp
+                print('Компьютер берёт', comp)
+                print('осталось', sweets)
+                corch = 1
+        elif 30 < sweets <= 58:
+            comp = sweets - 30
+            sweets -= comp
+            print('Компьютер берёт', comp)
+            print('осталось', sweets)
+            corch = 1
+        elif sweets == 30:
+            comp = 1
+            sweets -= comp
+            print('Компьютер берёт', comp)
+            print('осталось', sweets)
+            corch = 1
+        elif sweets <= 29:
+            comp = sweets - 1
+            if comp >= 1:
+                sweets -= comp
+                print('Компьютер берёт', comp)
+                print('осталось', sweets)
+                corch = 1
+            else:
+                comp += 1
+                sweets -= comp
+                print('Компьютер берёт', comp)
+                print('осталось', sweets)
+                corch = 1
+if corch == 0:
+    print('Проиграл игрок')
+elif corch == 1:
+    print('Бот проиграл')
